8d6bead5b8a26b8e12189c80f4f918d69c44e1fa
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2025-12-01 14:49:39 -0800

    rename "Notebook Jobs" to "Jobs" everywhere, remove mention of notebooks

--- src/index.tsx
+++ src/index.tsx
@@ -272,7 +272,7 @@ function activatePlugin(
       });
       mainAreaWidget.id = NotebookJobsPanelId;
       mainAreaWidget.title.icon = calendarMonthIcon;
-      mainAreaWidget.title.label = trans.__('Notebook Jobs');
+      mainAreaWidget.title.label = trans.__('Jobs');
       mainAreaWidget.title.closable = true;
     }
 
@@ -310,7 +310,7 @@ function activatePlugin(
       const filePath =
         getSelectedFilePath(widget, app.serviceManager.contents) ?? '';
 
-      // Update the job form inside the notebook jobs widget
+      // Update the job form inside the jobs widget
       const newCreateModel = emptyCreateJobModel();
       newCreateModel.inputFile = filePath;
       newCreateModel.jobName = MakeNameValid(
@@ -323,7 +323,7 @@ function activatePlugin(
         createJobModel: newCreateModel
       });
     },
-    label: trans.__('Create Notebook Job'),
+    label: trans.__('Create Job'),
     icon: calendarAddOnIcon
   });
 
@@ -336,7 +336,7 @@ function activatePlugin(
         getSelectedFilePath(widget, app.serviceManager.contents) ?? '';
       const fileName = getSelectedFileBaseName(widget) ?? '';
 
-      // Update the job form inside the notebook jobs widget
+      // Update the job form inside the jobs widget
       const newCreateModel = emptyCreateJobModel();
       newCreateModel.inputFile = filePath;
       newCreateModel.jobName = MakeNameValid(fileName);
@@ -347,7 +347,7 @@ function activatePlugin(
         createJobModel: newCreateModel
       });
     },
-    label: trans.__('Create a notebook job'),
+    label: trans.__('Create a job'),
     icon: calendarAddOnIcon
   });
 
@@ -386,7 +386,7 @@ function activatePlugin(
           jobsView: JobsView.ListJobs
         });
       },
-      label: trans.__('Notebook Jobs'),
+      label: trans.__('Jobs'),
       icon: eventNoteIcon
     });
 
